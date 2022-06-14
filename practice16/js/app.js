4ced41c1b69e156101b296c4230c3871e5f5345f
Rikiya Okawa <83910358+Rikiya89@users.noreply.github.com>
2022-06-14 22:12:07 +0900

    Add files via upload

--- practice16/js/app.js
+++ practice16/js/app.js
@@ -49,3 +49,44 @@ const nextBtn = document.querySelector(".next-btn");
 const randomBtn = document.querySelector(".random-btn");
 
 //set starting item
+let currentItem = 0;
+
+// load intial item
+window.addEventListener('DOMContentLoaded', function () {
+showPerson(currentItem);
+});
+
+// show person based on item
+function showPerson(person) {
+    const item = reviews[person];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+}
+
+// show next person 
+
+nextBtn.addEventListener('click', function (){
+    currentItem++;
+    if(currentItem > reviews.length - 1){
+        currentItem = 0;
+    }
+    showPerson(currentItem);
+});
+
+// show prev person 
+
+prevBtn.addEventListener('click', function (){
+    currentItem--;
+    if(currentItem < 0){
+        currentItem = reviews.length -1;
+    }
+    showPerson(currentItem);
+});
+
+// show ranodme person 
+randomBtn.addEventListener('click',function () {
+    currentItem = Math.floor(Math.random() * reviews.length);
+    showPerson(currentItem);
+});
